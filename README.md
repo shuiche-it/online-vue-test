b43c0a1d1c37a45131b160037ae0d302bbe538ad
shuiche-it <327896777@qq.com>
2022-03-29 15:42:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-6fus3f
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-6fus3f)
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="test"></div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  .test {
+    width: 100px;
+    height: 100px;
+    background: #ccc;
+  }
+}
+</style>
